57258ccd7e443fecb932f675e36d8e9b6408f650
Carter Cook <13cjcc@gmail.com>
2017-05-05 11:36:05 -0400

    changes from a long time ago, not sure

--- Assets/ThirdPersonUserControl.cs
+++ Assets/ThirdPersonUserControl.cs
@@ -1,52 +1,59 @@
 using System;
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
-using System.Collections.Generic;
 using Curves;
-using UnityStandardAssets.Characters.ThirdPerson;
-[RequireComponent(typeof(ThirdPersonCharacter))]
+
 public class ThirdPersonUserControl : MonoBehaviour
 {
 	public BezierSpline spline;
 	public float speed;
 	private float t = 0;
 
-	private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
-	Rigidbody m_Rigidbody;
-	Animator m_Animator;
+	Rigidbody rigidBody;
+	Animator animator;
 
 	private void Start()
 	{
-		m_Character = GetComponent<ThirdPersonCharacter>();
-		m_Animator = GetComponentInChildren<Animator>();
-		m_Rigidbody = GetComponentInChildren<Rigidbody>();
+		animator = GetComponentInChildren<Animator>();
+		rigidBody = GetComponentInChildren<Rigidbody>();
 	}
 
 	// Fixed update is called in sync with physics
 	private void FixedUpdate() {
-		Vector3 splineTangent = Vector3.ProjectOnPlane(spline.GetVelocity(t), Vector3.up);
+		bool up = Input.GetKey(KeyCode.UpArrow);
+		bool down = Input.GetKey(KeyCode.DownArrow);
 
-		//advance t along the line
-		if (Input.GetKey(KeyCode.UpArrow)) {
-			t += speed/splineTangent.magnitude;
-		} else if (Input.GetKey(KeyCode.DownArrow)) {
-			t -= speed/splineTangent.magnitude;
+		Vector3 moveVector = Vector3.zero;
+
+		if (up || down) {
+			
+
+			Vector3 splineTangent = Vector3.ProjectOnPlane(spline.GetVelocity(t), Vector3.up);
+
+			//advance t along the line
+			float deltaT = 0;
+			if (up) {
+				deltaT = speed/splineTangent.magnitude;
+			} else if (down) {
+				deltaT = -speed/splineTangent.magnitude;
+			}
+
+			t += deltaT;
+
+			moveVector = spline.pointAt(t) - transform.position; //vector from character to spline point
+
+			//draw moveVector
+			Debug.DrawLine(transform.position, transform.position + moveVector, Color.red);
+
+			//project moveVector onto ground normal
+			moveVector = Vector3.ProjectOnPlane(moveVector, Vector3.up);
+
+			//apply movement and rotation
+			transform.position += moveVector;
+			transform.rotation = Quaternion.LookRotation(Math.Sign(deltaT)*splineTangent, Vector3.up);
+			Debug.Log("moveVector: "+moveVector+", y rotation: "+transform.rotation.eulerAngles.y);
 		}
 
-		Vector3 moveVector = spline.pointAt(t) - transform.position; //vector from character to spline point
-
-		//draw moveVector
-		Debug.DrawLine(transform.position, transform.position + moveVector, Color.red);
-
-		//project moveVector onto ground normal
-		moveVector = Vector3.ProjectOnPlane(moveVector, Vector3.up);
-
-		//apply movement and rotation
-		transform.position += moveVector;
-		transform.rotation = Quaternion.LookRotation(splineTangent, Vector3.up);
-		Debug.Log("moveVector: "+moveVector+", y rotation: "+transform.rotation.eulerAngles.y);
-
 		// update the animator parameters
-		m_Animator.SetFloat("Forward", moveVector.magnitude);
+		animator.SetFloat("Forward", moveVector.magnitude);
 	}
 }
